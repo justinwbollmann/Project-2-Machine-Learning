055320e4592be43c362d744c5740113ec609115f
Justin Wonjung Bollmann <32852883+justinwbollmann@users.noreply.github.com>
2022-12-07 00:26:11 -0800

    first commit, adding menu items

---
+++ Menu.py
@@ -0,0 +1,20 @@
+file = input("Type in the name of the file to test: ")
+
+dataset = []
+with open(file, 'r') as f:
+    for line in f:
+        thing = []
+        for number in line.split():
+            thing.append(float(number))
+        dataset.append(thing)
+
+
+features = len(dataset[0]) - 1
+
+algorithm = int(input("Enter algorithm choice: 1)Forward Selection 2) Backwards Elimination "))
+
+if algorithm == 1:
+    forwardSelection(dataset, features)
+
+if algorithm == 2:
+    backwardElimination(dataset, features)
